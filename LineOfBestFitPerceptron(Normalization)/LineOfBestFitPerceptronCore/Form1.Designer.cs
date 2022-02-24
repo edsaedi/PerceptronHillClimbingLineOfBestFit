41c0a0fba55db6147704fa114659897aeb33b197
GMR Student <gmrstudent@outlook.com>
2022-02-23 18:23:46 -0800

    Fix the UI. All the code works. It is just displaying the y-intercept poorly

---
+++ LineOfBestFitPerceptron(Normalization)/LineOfBestFitPerceptronCore/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace LineOfBestFitPerceptronCoreNormalization
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvasPictureBox = new System.Windows.Forms.PictureBox();
+            this.slopeLabel = new System.Windows.Forms.Label();
+            this.yInterceptLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.RandomAdd = new System.Windows.Forms.Button();
+            this.LinearRegression = new System.Windows.Forms.Button();
+            this.xValue = new System.Windows.Forms.NumericUpDown();
+            this.yValue = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvasPictureBox
+            // 
+            this.canvasPictureBox.Location = new System.Drawing.Point(46, 44);
+            this.canvasPictureBox.Name = "canvasPictureBox";
+            this.canvasPictureBox.Size = new System.Drawing.Size(449, 382);
+            this.canvasPictureBox.TabIndex = 0;
+            this.canvasPictureBox.TabStop = false;
+            this.canvasPictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvasPictureBox_MouseClick);
+            // 
+            // slopeLabel
+            // 
+            this.slopeLabel.AutoSize = true;
+            this.slopeLabel.Enabled = false;
+            this.slopeLabel.Location = new System.Drawing.Point(518, 73);
+            this.slopeLabel.Name = "slopeLabel";
+            this.slopeLabel.Size = new System.Drawing.Size(39, 15);
+            this.slopeLabel.TabIndex = 1;
+            this.slopeLabel.Text = "Slope:";
+            this.slopeLabel.Visible = false;
+            // 
+            // yInterceptLabel
+            // 
+            this.yInterceptLabel.AutoSize = true;
+            this.yInterceptLabel.Enabled = false;
+            this.yInterceptLabel.Location = new System.Drawing.Point(520, 118);
+            this.yInterceptLabel.Name = "yInterceptLabel";
+            this.yInterceptLabel.Size = new System.Drawing.Size(69, 15);
+            this.yInterceptLabel.TabIndex = 2;
+            this.yInterceptLabel.Text = "Y-intercept:";
+            this.yInterceptLabel.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(668, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input a point:";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Location = new System.Drawing.Point(631, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "x:";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.Location = new System.Drawing.Point(631, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "y:";
+            this.label3.Visible = false;
+            // 
+            // Add
+            // 
+            this.Add.Enabled = false;
+            this.Add.Location = new System.Drawing.Point(631, 143);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(141, 79);
+            this.Add.TabIndex = 8;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Visible = false;
+            // 
+            // RandomAdd
+            // 
+            this.RandomAdd.Enabled = false;
+            this.RandomAdd.Location = new System.Drawing.Point(631, 228);
+            this.RandomAdd.Name = "RandomAdd";
+            this.RandomAdd.Size = new System.Drawing.Size(141, 79);
+            this.RandomAdd.TabIndex = 9;
+            this.RandomAdd.Text = "Add a Random Point";
+            this.RandomAdd.UseVisualStyleBackColor = true;
+            this.RandomAdd.Visible = false;
+            // 
+            // LinearRegression
+            // 
+            this.LinearRegression.Location = new System.Drawing.Point(631, 325);
+            this.LinearRegression.Name = "LinearRegression";
+            this.LinearRegression.Size = new System.Drawing.Size(141, 79);
+            this.LinearRegression.TabIndex = 10;
+            this.LinearRegression.Text = "Regress";
+            this.LinearRegression.UseVisualStyleBackColor = true;
+            this.LinearRegression.Click += new System.EventHandler(this.LinearRegression_Click);
+            // 
+            // xValue
+            // 
+            this.xValue.Enabled = false;
+            this.xValue.Location = new System.Drawing.Point(652, 66);
+            this.xValue.Name = "xValue";
+            this.xValue.Size = new System.Drawing.Size(120, 23);
+            this.xValue.TabIndex = 11;
+            this.xValue.Visible = false;
+            // 
+            // yValue
+            // 
+            this.yValue.Enabled = false;
+            this.yValue.Location = new System.Drawing.Point(652, 92);
+            this.yValue.Name = "yValue";
+            this.yValue.Size = new System.Drawing.Size(120, 23);
+            this.yValue.TabIndex = 12;
+            this.yValue.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.yValue);
+            this.Controls.Add(this.xValue);
+            this.Controls.Add(this.LinearRegression);
+            this.Controls.Add(this.RandomAdd);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yInterceptLabel);
+            this.Controls.Add(this.slopeLabel);
+            this.Controls.Add(this.canvasPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.canvasPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvasPictureBox;
+        private System.Windows.Forms.Label slopeLabel;
+        private System.Windows.Forms.Label yInterceptLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button RandomAdd;
+        private System.Windows.Forms.Button LinearRegression;
+        private System.Windows.Forms.NumericUpDown xValue;
+        private System.Windows.Forms.NumericUpDown yValue;
+    }
+}
+
---
+++ LineOfBestFitPerceptron(Normalization)/LineOfBestFitPerceptronCore/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PerceptronHillClimberLibrary;
+
+namespace LineOfBestFitPerceptronCoreNormalization
+{
+    public partial class Form1 : Form
+    {
+        public static List<(decimal, decimal)> dataPoints;
+        Bitmap canvas;
+        Graphics gfx;
+        int originX;
+        int originY;
+        HashSet<Point> uniqueDataPoints = new HashSet<Point>();
+        Perceptron perceptron;
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataPoints = new List<(decimal, decimal)>();
+            canvas = new Bitmap(canvasPictureBox.Width, canvasPictureBox.Height);
+            originX = canvasPictureBox.Width / 2;
+            originY = canvasPictureBox.Width / 2;
+            gfx = Graphics.FromImage(canvas);
+            Graph();
+
+            xValue.Minimum = -originX + 5;
+            xValue.Maximum = originX - 5;
+            yValue.Minimum = -originY + 5;
+            yValue.Maximum = originY - 5;
+
+            random = new Random();
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void canvasPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            var location = e.Location;
+            //this makes sure that you do not have duplicates
+            if (uniqueDataPoints.Add(location) == false)
+            {
+                return;
+            }
+
+            //gfx.FillEllipse(Brushes.Black, location.X - 5, location.Y - 5, 10, 10);
+            dataPoints.Add((location.X - 5, location.Y - 5));
+            Graph();
+            //canvasPictureBox.Image = canvas;
+        }
+
+        public static decimal LinearEquationSolver(decimal x, decimal M, decimal B)
+        {
+            return (x * M) + B;
+        }
+
+        public static double MeanSquaredError(double[] output, double[] desiredOutput)
+        {
+            if (output.Length != desiredOutput.Length || output.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            //return Math.Pow(desiredOutput - output, 2);
+
+            double error = 0;
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                error += Math.Pow(desiredOutput[i] - output[i], 2);
+            }
+
+            return error / output.Length;
+        }
+
+        public static double MeanSquaredError(double output, double desiredOutput)
+        {
+            return Math.Pow((desiredOutput - output), 2);
+        }
+
+        double RegressionError(List<(decimal, decimal)> points)
+        {
+            //error is mean of the squares of residuals
+            //where the residual is y actual - y predicted from x actual
+            double[] yValues = points.Select(p => (double)p.Item2).ToArray();
+            return MeanSquaredError(points.Select(p => (double)p.Item1).Select(x => perceptron.Compute(new double[] { x })).ToArray(), yValues);
+        }
+
+        public decimal NormalizePoint(decimal min, decimal max, decimal nMin, decimal nMax, decimal value)
+        {
+            decimal part1 = (value - min) / (max - min);
+            decimal part2 = (nMax - nMin);
+            decimal part12 = part1 * part2;
+            return part12 + nMin;
+        }
+
+        public decimal UnNormalizePoint(decimal min, decimal max, decimal nMin, decimal nMax, decimal value)
+        {
+            decimal part1 = (value - nMin) / (nMax - nMin);
+            decimal part2 = (max) - (min);
+            decimal part12 = part1 * part2;
+            return part12 + min;
+        }
+
+
+        public List<(decimal, decimal)> NormalizeData(decimal nMinX, decimal nMaxX, decimal nMinY, decimal nMaxY, List<(decimal, decimal)> dataPoints)
+        {
+            decimal minX = dataPoints.Select(p => p.Item1).Min();
+            decimal maxX = dataPoints.Select(p => p.Item1).Max();
+
+            decimal minY = dataPoints.Select(p => p.Item2).Min();
+            decimal maxY = dataPoints.Select(p => p.Item2).Max();
+
+            //nMin and nMax are the minimum and maximums that you want as a programmer
+
+            List<(decimal, decimal)> normalizedPoints = new List<(decimal, decimal)>();
+            foreach ((decimal x, decimal y) in dataPoints)
+            {
+                normalizedPoints.Add((NormalizePoint(minX, maxX, nMinX, nMaxX, x), NormalizePoint(minY, maxY, nMinY, nMaxY, y)));
+            }
+
+            //Algorithm part
+            return normalizedPoints;
+        }
+
+        public List<(decimal, decimal)> UnNormalizeData(decimal minX, decimal maxX, decimal minY, decimal maxY, List<(decimal, decimal)> normalizedData)
+        {
+            decimal nMinX = normalizedData.Select(p => p.Item1).Min();
+            decimal nMaxX = normalizedData.Select(p => p.Item1).Max();
+
+            decimal nMinY = normalizedData.Select(p => p.Item2).Min();
+            decimal nMaxY = normalizedData.Select(p => p.Item2).Max();
+
+            List<(decimal, decimal)> unNormalizedPoints = new List<(decimal, decimal)>();
+            foreach ((decimal x, decimal y) in normalizedData)
+            {
+                unNormalizedPoints.Add((UnNormalizePoint(minX, maxX, nMinX, nMaxX, x), UnNormalizePoint(minY, maxY, nMinY, nMaxY, y)));
+            }
+
+            return unNormalizedPoints;
+        }
+
+        private void LinearRegression_Click(object sender, EventArgs e)
+        {
+
+            perceptron = new Perceptron(1, 0.1, random, MeanSquaredError);
+
+            //Rest of the code for the linear regression. Use the library we have.
+
+            LinearRegression.Enabled = false;
+
+            
+            var normalizedPoints = NormalizeData(0, 1, 0, 1, dataPoints);
+
+            //while (iterations <= 1000000 && perceptron.GetError(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2)) > 1)
+            //{
+            //    //perceptron.TrainWithHillClimbing(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2), perceptron.GetError(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2)));
+            //    perceptron.TrainWithHillClimbing(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2), perceptron.GetError(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2)));
+
+            //    iterations++;
+            //}
+
+            int iterations = 0;
+
+            while (iterations <= 1000000 && perceptron.GetError(DoubleConversion(normalizedPoints), SingleConversion(normalizedPoints, 2)) > 0.01)
+            {
+                //perceptron.TrainWithHillClimbing(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2), perceptron.GetError(DoubleConversion(dataPoints), SingleConversion(dataPoints, 2)));
+                perceptron.TrainWithHillClimbing(DoubleConversion(normalizedPoints), SingleConversion(normalizedPoints, 2), perceptron.GetError(DoubleConversion(normalizedPoints), SingleConversion(normalizedPoints, 2)));
+
+                iterations++;
+
+            }
+
+            //keep in mind that the bias is the y-intercept and the weights are the slope
+            //make sure to repeat the hill climbing until you get within a certain amount of error or iterations
+
+            GraphLineNormalized();
+
+            LinearRegression.Enabled = true;
+        }
+
+
+        private double[][] DoubleConversion(List<(decimal, decimal)> list)
+        {
+            return (list.Select(p => (double)p.Item1).Select(x => new double[] { x }).ToArray());
+        }
+
+        private double[] SingleConversion(List<(decimal, decimal)> list, int itemNumber = 1)
+        {
+            if (itemNumber == 1)
+            {
+                return list.Select(p => (double)p.Item1).ToArray();
+            }
+
+            return list.Select(p => (double)p.Item2).ToArray();
+        }
+
+        public void Graph()
+        {
+            gfx.Clear(Color.White);
+            gfx.DrawLine(new Pen(Color.Black), new Point(0, originY), new Point(canvasPictureBox.Width, originY));
+            gfx.DrawLine(new Pen(Color.Black), new Point(originX, 0), new Point(originX, canvasPictureBox.Height));
+
+            foreach (var point in dataPoints)
+            {
+                gfx.FillEllipse(Brushes.Black, (float)point.Item1, (float)point.Item2, 10, 10);
+            }
+
+            canvasPictureBox.Image = canvas;
+        }
+
+        public void GraphNormalized(List<(decimal, decimal)> normalizedPoints)
+        {
+            gfx.Clear(Color.White);
+            
+            //Axes lines
+            gfx.DrawLine(new Pen(Color.Black), new Point(0, originY), new Point(canvasPictureBox.Width, originY));
+            gfx.DrawLine(new Pen(Color.Black), new Point(originX, 0), new Point(originX, canvasPictureBox.Height));
+
+            var unNormalizedPoints = UnNormalizeData(0, canvasPictureBox.Width, 0, canvasPictureBox.Height, normalizedPoints);
+
+            foreach (var point in unNormalizedPoints)
+            {
+                gfx.FillEllipse(Brushes.Black, (float)point.Item1, (float)point.Item2, 10, 10);
+            }
+
+            canvasPictureBox.Image = canvas;
+        }
+
+        public void GraphLineNormalized()
+        {
+            decimal slope = (decimal)perceptron.weights[0];
+            //Note that if the function is increasing, it will have a negative slope because the y-coordinates are flipped in this type of window forms.
+            //Remeber that the coordinate (0,0) is at the top left corner with x increasing as it moves to the right and y increasing as it moves down.
+            //there is only one value in our weights array and that is the slope.
+            decimal yInterceptNormalized = (decimal)perceptron.bias;
+            //decimal yIntercept = UnNormalizePoint(0, canvasPictureBox.Height, 0, 1, yInterceptNormalized);
+
+            decimal min = dataPoints.Select(p => p.Item2).Min();
+            decimal max = dataPoints.Select(p => p.Item2).Max();
+
+            decimal yIntercept = UnNormalizePoint(min, max, 0, 1, yInterceptNormalized);
+
+            //the bias is the y-intercept
+
+            gfx.DrawLine(new Pen(Color.Black), new Point(0, (int)LinearEquationSolver(0, slope, yIntercept)), new Point(canvasPictureBox.Width, (int)LinearEquationSolver(canvasPictureBox.Width, slope, yIntercept)));
+
+            canvasPictureBox.Image = canvas;
+        }
+
+        public void GraphLine()
+        {
+            decimal slope = (decimal)perceptron.weights[0];
+            //Note that if the function is increasing, it will have a negative slope because the y-coordinates are flipped in this type of window forms.
+            //Remeber that the coordinate (0,0) is at the top left corner with x increasing as it moves to the right and y increasing as it moves down.
+            //there is only one value in our weights array and that is the slope.
+            decimal yIntercept = (decimal)perceptron.bias;
+            //the bias is the y-intercept
+
+            gfx.DrawLine(new Pen(Color.Black), new Point(0, (int)LinearEquationSolver(0, slope, yIntercept)), new Point(canvasPictureBox.Width, (int)LinearEquationSolver(canvasPictureBox.Width, slope, yIntercept)));
+
+            canvasPictureBox.Image = canvas;
+        }
+    }
+}
